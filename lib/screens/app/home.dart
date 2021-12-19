b50499ce38807326d588f0709ad29276c0b14220
Omicrxn <alexavila0128@gmail.com>
2021-12-19 13:49:22 +0100

    Top carousel added on home screen

--- lib/screens/app/home.dart
+++ lib/screens/app/home.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutters_of_hamelin/assets.dart';
 import 'package:flutters_of_hamelin/colors.dart';
@@ -24,7 +25,7 @@ class Home extends StatelessWidget {
             SliverFillRemaining(
               child: Column(
                 children: [
-                  const MainList(),
+                  MainList(),
                 ],
               ),
             )
@@ -33,28 +34,70 @@ class Home extends StatelessWidget {
   }
 }
 
-class MainList extends StatelessWidget {
-  const MainList({Key? key}) : super(key: key);
+class MainList extends StatefulWidget {
+  MainList({Key? key}) : super(key: key);
 
+  @override
+  State<MainList> createState() => _MainListState();
+}
+
+class _MainListState extends State<MainList> {
+  final CarouselController _carouselController = CarouselController();
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
       height: 100,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: const[
-          Text(
-            "Text1",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+      child: CarouselSlider(
+        options: CarouselOptions(
+            initialPage: 1,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            height: 100,
+            scrollDirection: Axis.horizontal,
+            viewportFraction: 0.3,
+            scrollPhysics: NeverScrollableScrollPhysics()),
+        carouselController: _carouselController,
+        items: [
+          GestureDetector(
+            onTap: () {
+              _carouselController.jumpToPage(0);
+            },
+            child: Text(
+              "Albums",
+              style: TextStyle(
+                  fontSize: _currentIndex == 0 ? 24 : 18,
+                  fontWeight:
+                      _currentIndex == 0 ? FontWeight.w700 : FontWeight.w600),
+            ),
           ),
-          Text(
-            "Text2",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+          GestureDetector(
+            onTap: () {
+              _carouselController.jumpToPage(1);
+            },
+            child: Text(
+              "Songs",
+              style: TextStyle(
+                  fontSize: _currentIndex == 1 ? 24 : 18,
+                  fontWeight:
+                      _currentIndex == 1 ? FontWeight.w700 : FontWeight.w600),
+            ),
           ),
-          Text(
-            "Text1",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          GestureDetector(
+            onTap: () {
+              _carouselController.jumpToPage(2);
+            },
+            child: Text(
+              "Artists",
+              style: TextStyle(
+                  fontSize: _currentIndex == 2 ? 24 : 18,
+                  fontWeight:
+                      _currentIndex == 2 ? FontWeight.w700 : FontWeight.w600),
+            ),
           ),
         ],
       ),
